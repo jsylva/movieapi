9815c2d112b508b3ab22b3a266aecd3c01a56a07
jsylva <62109355+jsylva@users.noreply.github.com>
2021-09-08 22:06:17 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,18 @@
 document.getElementById("search-btn").addEventListener("click", () => {
- searchTerm = document.getElementById("searchParameter").value;
-  
-  fetch(`https://www.omdbapi.com/?apikey=67ab0474&s=${searchTerm}`).then(response => response.json()).then(data => {
-    const movies = data.Search.map(movie => {
-      return `<img src="${movie.Poster}"  onclick="info('${movie.imdbID}')"/>`
-    }).join('');
-    document.getElementById('app').innerHTML = movies;
-  })
-  
+    searchTerm = document.getElementById("searchParameter").value;
+
+    fetch(`https://www.omdbapi.com/?apikey=67ab0474&s=${searchTerm}`).then(response => response.json()).then(data => {
+        const movies = data.Search.map(movie => {
+            return `<img src="${movie.Poster}"  onclick="info('${movie.imdbID}')"/>`
+        }).join('');
+        document.getElementById('app').innerHTML = movies;
+    })
+
 });
 
 
-  function info(imdb) {
-      fetch(`https://www.omdbapi.com/?apikey=67ab0474&i=${imdb}`).then(response => response.json()).then(data => {
-      document.getElementById('description').innerHTML = "Movie Title: " + data.Title + "<br />" + "The Movie Was Released: " + data.Released + "<br />" + "The Movie Is Rated: " + data.Rated + "<br />" + "Writer: " + data.Director + "<br />" + "Actors: " + data.Actors;
+function info(imdb) {
+    fetch(`https://www.omdbapi.com/?apikey=67ab0474&i=${imdb}`).then(response => response.json()).then(data => {
+        document.getElementById('description').innerHTML = " <h2>-Movie Information-</h2> <br/> Movie Title: " + data.Title + "<br />" + "The Movie Was Released: " + data.Released + "<br />" + "The Movie Is Rated: " + data.Rated + "<br />" + "Writer: " + data.Director + "<br />" + "Actors: " + data.Actors;
     })
- }
-  
-
+}
